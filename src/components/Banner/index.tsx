4426ea8e6f449d9daeb6890d9c2489795d063c55
Eduardo <eduardoantune12@gmail.com>
2025-07-11 15:15:38 -0300

    Componentize Parte I

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,14 +1,23 @@
 import { Imagem, Titulo, Preco } from './styles'
 
 import bannerImg from '../../assets/images/banner-homem-aranha.png'
+import Tag from '../Tag'
+import Button from '../Button'
 
 const Banner = () => (
   <Imagem style={{ backgroundImage: `url(${bannerImg})` }}>
     <div className="container">
-      <Titulo>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Titulo>
-      <Preco>
-        De <s>R$ 250,00</s> <br /> por apenas R$ 99,90
-      </Preco>
+      <Tag size="large">Destaque do dia</Tag>
+      <div>
+        <Titulo>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Titulo>
+        <Preco>
+          De <s>R$ 250,00</s> <br />
+          por apenas R$ 99,90
+        </Preco>
+      </div>
+      <Button type="link" to="/produto" title="Clique e compre agora">
+        De o play
+      </Button>
     </div>
   </Imagem>
 )
