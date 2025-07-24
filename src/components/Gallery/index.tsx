d75160be3f912f42fd1b0f94799c0ee08a284ea3
Eduardo <eduardoantune12@gmail.com>
2025-07-23 21:40:28 -0300

    Criando galeria - Parte II - Criando Modal e sua estilização

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,21 +1,79 @@
 import Section from '../Section'
+import { GalItem, GalList, Action, Modal, ModalContent } from './styles'
 import jedi from '../../assets/images/star_wars.png'
-import { GalItem, GalList } from './styles'
+import spiderman from '../../assets/images/banner-homem-aranha.png'
+import play from '../../assets/images/play.png'
+import zoom from '../../assets/images/zoom.png'
+import fechar from '../../assets/images/fechar.png'
 
-const Gallery = () => (
-  <Section title="Galeria" background="darkBlue">
-    <GalList>
-      <GalItem>
-        <img src={jedi} alt="imagem do link" />
-      </GalItem>
-      <GalItem>
-        <img src={jedi} alt="imagem do link" />
-      </GalItem>
-      <GalItem>
-        <img src={jedi} alt="imagem do link" />
-      </GalItem>
-    </GalList>
-  </Section>
-)
+type GalleryItem = {
+  type: 'image' | 'video'
+  url: string
+  name: string
+}
+
+const mock: GalleryItem[] = [
+  {
+    type: 'image',
+    url: jedi
+  },
+  {
+    type: 'image',
+    url: spiderman
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/1O6Qstncpnc?si=88TP_RCre8mcg8CX&amp;start=3'
+  }
+]
+
+type Props = {
+  defaultCover: () => string
+}
+
+const Gallery = ({ defaultCover }: Props) => {
+  const getMediaCover = (item: GalleryItem) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+
+  const getMediaIcon = (item: GalleryItem) => {
+    if (item.type === 'image') return zoom
+    return play
+  }
+
+  return (
+    <>
+      <Section title="Galeria" background="darkBlue">
+        <GalList>
+          {mock.map((media, index) => (
+            <GalItem key={media.url}>
+              <img
+                src={getMediaCover(media)}
+                alt={`Mídia ${index + 1} de ${media.name}`}
+              />
+              <Action>
+                <img
+                  src={getMediaIcon(media)}
+                  alt="Clique para maximizar a mídia"
+                />
+              </Action>
+            </GalItem>
+          ))}
+        </GalList>
+      </Section>
+      <Modal>
+        <ModalContent className="container">
+          <header>
+            <h4>Jogo Teste</h4>
+            <img src={fechar} alt="Ícone de fechar" />
+          </header>
+          <img src={spiderman} />
+        </ModalContent>
+        <div className="overlay" />
+      </Modal>
+    </>
+  )
+}
 
 export default Gallery
--- src/components/Gallery/styles.ts
+++ src/components/Gallery/styles.ts
@@ -4,15 +4,79 @@ import { cores } from '../../styles'
 export const GalList = styled.ul`
   display: flex;
 `
+export const Action = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #000000ba;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0;
+`
 
 export const GalItem = styled.li`
   margin-right: 16px;
+  position: relative;
 
-  img {
+  > img {
     border-radius: 8px;
     border: 2px solid ${cores.branco};
     width: 150px;
     height: 150px;
     object-fit: cover;
+  }
+
+  &:hover {
+    ${Action} {
+      opacity: 1;
+      transition: opacity 0.5s ease;
+      cursor: pointer;
+    }
+  }
+`
+
+export const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #000000ba;
+  }
+`
+
+export const ModalContent = styled.div`
+  max-width: 960px;
+  position: relative;
+  z-index: 1;
+
+  header {
+    display: flex;
+    margin-bottom: 24px;
+    justify-content: space-between;
+
+    h4 {
+      font-size: 18px;
+      font-weight: 700;
+    }
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
   }
 `
