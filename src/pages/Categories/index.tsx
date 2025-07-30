48f05ce1c050c2731676888e4d93f3bcbcf780b1
Eduardo <eduardoantune12@gmail.com>
2025-07-30 15:35:15 -0300

    Fazendo requisições API com RTK Query

--- src/pages/Categories/index.tsx
+++ src/pages/Categories/index.tsx
@@ -1,53 +1,46 @@
 import { ProductsList } from '../../components/ProductsList'
 
-import resident from '../../assets/images/resident.png'
-import diablo from '../../assets/images/diablo.png'
-import zelda from '../../assets/images/zelda.png'
-import starWars from '../../assets/images/star_wars.png'
-import { Game } from '../Home'
-import { useEffect, useState } from 'react'
+import {
+  useGetActionGamesQuery,
+  useGetSimulationGamesQuery,
+  useGetFightingGamesQuery,
+  useGetSportGamesQuery,
+  useGetRpgGamesQuery
+} from '../../services/api'
 
 const Categories = () => {
-  const [gamesAcao, setGamesAcao] = useState<Game[]>([])
-  const [gamesEsportes, setGamesEsportes] = useState<Game[]>([])
-  const [gamesSimulacao, setGamesSimulacao] = useState<Game[]>([])
-  const [gamesLuta, setGamesLuta] = useState<Game[]>([])
-  const [gamesRPG, setGamesRPG] = useState<Game[]>([])
+  const { data: actionGames } = useGetActionGamesQuery()
+  const { data: simulationGames } = useGetSimulationGamesQuery()
+  const { data: fightingGames } = useGetFightingGamesQuery()
+  const { data: sportGames } = useGetSportGamesQuery()
+  const { data: rpgGames } = useGetRpgGamesQuery()
 
-  useEffect(() => {
-    fetch('https://fake-api-tau.vercel.app/api/eplay/acao')
-      .then((res) => res.json())
-      .then((res) => setGamesAcao(res))
+  if (
+    actionGames &&
+    simulationGames &&
+    fightingGames &&
+    sportGames &&
+    rpgGames
+  ) {
+    return (
+      <>
+        <ProductsList games={actionGames} title="Ação" background="darkBlue" />
+        <ProductsList games={sportGames} title="Esportes" background="blue" />
+        <ProductsList
+          games={fightingGames}
+          title="Luta"
+          background="darkBlue"
+        />
+        <ProductsList games={rpgGames} title="RPG" background="blue" />
+        <ProductsList
+          games={simulationGames}
+          title="Simulação"
+          background="darkBlue"
+        />
+      </>
+    )
+  }
 
-    fetch('https://fake-api-tau.vercel.app/api/eplay/esportes')
-      .then((res) => res.json())
-      .then((res) => setGamesEsportes(res))
-
-    fetch('https://fake-api-tau.vercel.app/api/eplay/simulacao')
-      .then((res) => res.json())
-      .then((res) => setGamesSimulacao(res))
-
-    fetch('https://fake-api-tau.vercel.app/api/eplay/luta')
-      .then((res) => res.json())
-      .then((res) => setGamesLuta(res))
-
-    fetch('https://fake-api-tau.vercel.app/api/eplay/rpg')
-      .then((res) => res.json())
-      .then((res) => setGamesRPG(res))
-  }, [])
-
-  return (
-    <>
-      <ProductsList games={gamesAcao} title="Ação" background="darkBlue" />
-      <ProductsList games={gamesEsportes} title="Esportes" background="blue" />
-      <ProductsList games={gamesLuta} title="Luta" background="darkBlue" />
-      <ProductsList games={gamesRPG} title="RPG" background="blue" />
-      <ProductsList
-        games={gamesSimulacao}
-        title="Simulação"
-        background="darkBlue"
-      />
-    </>
-  )
+  return <h4>Carregando</h4>
 }
 export default Categories
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,7 @@
-import { useEffect, useState } from 'react'
 import { ProductsList } from '../../components/ProductsList'
 import Banner from '../../components/Banner'
 
-import resident from '../../assets/images/resident.png'
-import diablo from '../../assets/images/diablo.png'
-import zelda from '../../assets/images/zelda.png'
-import starWars from '../../assets/images/star_wars.png'
+import { useGetComingSoonQuery, useGetOnSaleQuery } from '../../services/api'
 
 export interface GalleryItem {
   type: 'image' | 'video'
@@ -38,26 +34,20 @@ export type Game = {
 }
 
 const Home = () => {
-  const [promocoes, setPromocoes] = useState<Game[]>([])
-  const [emBreve, setEmBreve] = useState<Game[]>([])
+  const { data: comingSoon } = useGetComingSoonQuery()
+  const { data: onSale } = useGetOnSaleQuery()
 
-  useEffect(() => {
-    fetch('https://fake-api-tau.vercel.app/api/eplay/promocoes')
-      .then((res) => res.json())
-      .then((res) => setPromocoes(res))
+  if (onSale && comingSoon) {
+    return (
+      <>
+        <Banner />
+        <ProductsList games={onSale} title="Promoções" background="darkBlue" />
+        <ProductsList games={comingSoon} title="Em breve" background="blue" />
+      </>
+    )
+  }
 
-    fetch('https://fake-api-tau.vercel.app/api/eplay/em-breve')
-      .then((res) => res.json())
-      .then((res) => setEmBreve(res))
-  }, [])
-
-  return (
-    <>
-      <Banner />
-      <ProductsList games={promocoes} title="Promoções" background="darkBlue" />
-      <ProductsList games={emBreve} title="Em breve" background="blue" />
-    </>
-  )
+  return <h4>Carregando</h4>
 }
 
 export default Home
